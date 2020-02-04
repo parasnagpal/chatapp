5b6947ae6e06b8bd258be12b1c405eb12bcdf84f
Paras Nagpal <paras.nag@gmail.com>
2020-02-05 04:03:33 +0530

    (style): formatting improvement

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const session=require('express-session')
 
 //Body Parser to set linit to req size
 const bodyparser=require('body-parser')
-//formidable
-
 
 //import from API folder
 const App=require('./API/post')
@@ -18,16 +16,12 @@ let LocalStrategy=require('passport-local').Strategy
 
 let tempChatData={}
 
-
 //Socket files
 const socketio=require('socket.io')
 const http=require('http')
 const app=express()
 const server=http.createServer(app)
 const io=socketio(server)
-
-//countpath
-
 
 //Peoples
 let map={}
@@ -36,50 +30,43 @@ let revmap={}
 let session_username_map=App.session_username_map
 let user_name
 
-
-
 app.use(bodyparser.json({limit:'2MB'}))
 app.use(bodyparser.urlencoded({
     extended:true,
     limit:'2MB'
 }))
 
-
 app.use(session({
     secret:'this is MY secret!!!!',
     resave:true,
     saveUninitialized:false,
     cookie:{
-      
-      maxAge:24*60*60*1000
+        maxAge:24*60*60*1000
     }
 }))
 
-
-
 //Socket Connections
 {
-   io.on('connection',(socket)=>{
-      map[socket.id]=user_name
-      mapAlive[user_name]=false
-      revmap[user_name]=socket.id
+    io.on('connection',(socket)=>{
+      map[socket.id]=user_name;
+      mapAlive[user_name]=false;
+      revmap[user_name]=socket.id;
       
 
       socket.on('msgfor',(data)=>{
-           io.to(revmap[data.name]).emit('incoming',{
-             from:map[socket.id],
-             message:data.message
+          io.to(revmap[data.name]).emit('incoming',{
+              from:map[socket.id],
+              message:data.message
             })
            //save chat to server
-           if(!tempChatData[data.name])
-            tempChatData[data.name]={}
+          if(!tempChatData[data.name])
+              tempChatData[data.name]={};
            
-           if(!tempChatData[data.name][map[socket.id]])
-             tempChatData[data.name][map[socket.id]]={}
+          if(!tempChatData[data.name][map[socket.id]])
+              tempChatData[data.name][map[socket.id]]={};
            
-           tempChatData[data.name][map[socket.id]][Date.now()]=data.message
-           
-           socket.emit('unread',tempChatData)
+          tempChatData[data.name][map[socket.id]][Date.now()]=data.message;
+          socket.emit('unread',tempChatData);
       })
       
 
@@ -89,22 +76,21 @@ app.use(session({
       })
       
       socket.on('received',(data)=>{
-        //clear chat stored on server
-        tempChatData[data.name][data.from]={}
+          //clear chat stored on server
+          tempChatData[data.name][data.from]={}
       })
 
       socket.on('isOnline',(data)=>{
-            socket.emit('online',{
-                name:data.name,
-                answer:mapAlive[data.name]
-            })
-            
+          socket.emit('online',{
+              name:data.name,
+              answer:mapAlive[data.name]
+          })     
       })
 
       socket.on('isAlive',(data)=>{
-        mapAlive[session_username_map[data.session]]=true
-        revmap[session_username_map[data.session]]=socket.id
-        map[socket.id]=session_username_map[data.session]
+          mapAlive[session_username_map[data.session]]=true
+          revmap[session_username_map[data.session]]=socket.id
+          map[socket.id]=session_username_map[data.session]
       })
       
       io.to(socket.id).emit('identity',{
@@ -114,35 +100,35 @@ app.use(session({
 
    })
 
-   setInterval(deactivate,10000)
+   setInterval(deactivate,10000);
 
    function deactivate(){
      for(let obj in revmap)
-       mapAlive[obj]=false
+       mapAlive[obj]=false;
      }
 }
 
 //Configuration
 {
-app.set('view engine','hbs')
+  app.set('view engine','hbs')
 
-
-app.use((req,res,next)=>{
+  app.use((req,res,next)=>{
     
-    if(!req.session.user)
-      {
+    if(!req.session.user){
         //console.log("New user")
         req.session.user=true;
         req.session.logged=false;
-        return res.redirect('/login')
-      }  
-      else  next()
-   })
+        return res.redirect('/login');
+    }  
+    else  
+        next();
+        
+  })
 
-   app.use(express.static(__dirname+'/views'))
-   app.use(express.static(__dirname+'/chat'))
-   app.use('/',App.app)
-   app.use('/',require('./views/routes/route'))
+  app.use(express.static(__dirname+'/views'));
+  app.use(express.static(__dirname+'/chat'));
+  app.use('/',App.app);
+  app.use('/',require('./views/routes/route'));
 
 }
 
