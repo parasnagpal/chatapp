70c4c92739ce31c0f1a450107a2968fb1ba99611
Paras Nagpal <paras.nag@gmail.com>
2020-02-14 03:08:37 +0530

    (style): Formatting change

--- views/routes/user/script.js
+++ views/routes/user/script.js
@@ -13,26 +13,25 @@ $(document).ready(()=>{
    
    //Get Identity from server
    $.post('identity',{},(data)=>{
-     sessionID=data
-     console.log(sessionID)
-     setCookie('session',data,1);
+      sessionID=data;
+      setCookie('session',data,1);
    })
 
    $.post('myName',
-        {session:sessionID},
-        (data)=>{
-          myName=data
-          console.log(myName)
-   }).then(()=>{
-    socket.emit('unread')
+      {session:sessionID},
+      (data)=>{
+        myName=data
+      })
+   .then(()=>{
+    socket.emit('unread');
    })
 
-    let newMessageCount={}
-    let chats=[]
-    let friendsOnline={}
+    let newMessageCount={};
+    let chats=[];
+    let friendsOnline={};
     let friend;
     if(localStorage.arr)
-        chats=JSON.parse(localStorage.arr) 
+        chats=JSON.parse(localStorage.arr);
 
     for(let people of chats)
      {
@@ -41,23 +40,21 @@ $(document).ready(()=>{
         userdata[people]=data
       })    
      }
-    $('#error').hide()
-    //Insert friends
+    $('#error').hide();
         
     //update List
-    updatelist(chats)
+    updatelist(chats);
      
-
     //Friend Search
     $('#friend').keyup((e)=>{
-      $('#error').hide()
+      $('#error').hide();
         if(e.keyCode==13)
-          findfriend()
+          findfriend();
         
     })
     $('#chatline button').click(()=>{
-      $('#error').hide()       
-      findfriend()
+      $('#error').hide();      
+      findfriend();
     })
 
     function findfriend(){
@@ -71,38 +68,34 @@ $(document).ready(()=>{
         })
     }
 
-
     function updatechats(){
-        localStorage.arr=JSON.stringify(chats)
+        localStorage.arr=JSON.stringify(chats);
     }
 
-    
     function updatelist(arr){
        
       for(let a of arr) 
       { 
-        let image='https://image.flaticon.com/icons/png/512/37/37943.png'
+        let image='https://image.flaticon.com/icons/png/512/37/37943.png';   //hardcoded
         $('#chats')
-           .append(card(a,image,userdata[a]))
+           .append(card(a,image,userdata[a]));
            
-         //check unread
-         if(unreaddata)
-         if(unreaddata[myName])
-          if(unreaddata[myName][a])
-           {
-             $('#'+a+" span").html(`
-                             <span class="badge badge-pill badge-danger">New Message</span>
-                             `)
-           }
+        //check unread
+        if(unreaddata)
+          if(unreaddata[myName])
+            if(unreaddata[myName][a])
+            {
+              $('#'+a+" span").html(`<span class="badge badge-pill badge-danger">New Message</span>`);
+            }
         //Check if photo request has been made earlier
         if(!checkphoto[a])
          {
            //Fetch photo
            fetch('/photo',{
-                            method:'POST'
-                           ,body:JSON.stringify({'name':a}),
-                           headers: {"Content-Type": "application/json"}
-                          })
+                method:'POST',
+                body:JSON.stringify({'name':a}),
+                headers: {"Content-Type": "application/json"}
+              })
             .then(function(response) {
                 if(response.ok) {
                   return response.blob();    //convert response to blob - blob constructor
@@ -110,10 +103,10 @@ $(document).ready(()=>{
              throw new Error('Network response was not ok.');
              })
              .then(function(myBlob) { 
-                    let reader=new FileReader()
-                    reader.readAsDataURL(myBlob)
+                    let reader=new FileReader();
+                    reader.readAsDataURL(myBlob);
                     reader.onloadend=()=>{
-                    checkphoto[a]=reader.result
+                    checkphoto[a]=reader.result;
               }
              }).catch(function(error) {
                console.log('There has been a problem with your fetch operation: ', error.message);
@@ -121,49 +114,51 @@ $(document).ready(()=>{
          }
          
          if(checkphoto[a])
-         {
-           if(checkphoto[a].slice(5,9)!='text')
-            $(`#${a} img`).attr('src',checkphoto[a])
-         }
+          {
+            if(checkphoto[a].slice(5,9)!='text')
+              $(`#${a} img`).attr('src',checkphoto[a]);
+          }
 
          if(friendsOnline[a])
-         {
-          $('#'+a).attr('class','alert alert-danger mx-1')
-          $(`#btn-`+a).attr('class','chat-btns btn btn-outline-danger')
-         }
+          {
+            $('#'+a).attr('class','alert alert-danger mx-1');
+            $(`#btn-`+a).attr('class','chat-btns btn btn-outline-danger');
+          }
          if(newMessageCount[a])
           {
-            $(`#${a} .friends`).attr('value',a+'    ----'+newMessageCount[a]+'New Messages')
+            $(`#${a} .friends`).attr('value',a+'    ----'+newMessageCount[a]+'New Messages');
           }
       }
     }
 
     function refreshlist(friend){
-      let isNew=true
+      let isNew=true;
       //check if the friend exists in list
       for(let people of chats)
-       if(people==friend)
-        isNew=false;
+        if(people==friend)
+          isNew=false;
+
       if(isNew && friend!=myName)
       {  
-        chats.push(friend)
-        updatechats()
-        let fr=[]
-        fr.push(friend) 
-        updatelist(fr)
+          chats.push(friend);
+          updatechats();
+          let fr=[];
+          fr.push(friend);
+          updatelist(fr);
       }
-      else $('#error').html('Friend is already in your list!!').show()
+      else $('#error').html('Friend is already in your list!!').show();
+
     }
 
     $('#form').on('submit',(e)=>{
-       e.preventDefault()
+       e.preventDefault();
        //set value of hide box
        $('#send').val(globalState);
-       $('#form')[0].submit()
+       $('#form')[0].submit();
     })
     
     //Hide send box
-     $("#send").hide()
+     $("#send").hide();
 
     //Listening to messages
     socket.on('incoming',(data)=>{
@@ -173,8 +168,8 @@ $(document).ready(()=>{
      //Check if online
      function isOnline()
      {
-       for(let people of chats)
-        socket.emit('isOnline',{name:people})
+        for(let people of chats)
+          socket.emit('isOnline',{name:people});
      }
 
     socket.on('online',(data)=>{
@@ -185,43 +180,39 @@ $(document).ready(()=>{
        updatelist(chats)
     })
     
-    
-
-
     //Pinging Server that I am online
-     setInterval(()=>{
-       socket.emit('isAlive',{
-         session:sessionID
-       }) 
+    setInterval(()=>{
+      socket.emit('isAlive',{
+        session:sessionID
+      }) 
     },1000)
+
     //Check if friends are online
-     setInterval(()=>isOnline()
-     ,3000)
+    setInterval(()=>isOnline()
+    ,3000)
 
 
     //get unread data
     socket.emit('unread') 
     socket.on('unread',(data)=>{
-          unreaddata=data
+          unreaddata=data;
           
           if(data)
-           if(data[myName])
-           {
-            for(let x in data[myName])
-            {
-             
-             if(!chats.find((name)=>{ return name==x}))
-             {
-              console.log(x)
-              chats.push(x)
-              updatechats()
-              let fr=[]
-              fr.push(x) 
-              updatelist(fr)
-             }
-            }
-           } 
-           /*if(!chats.find(people))
+            if(data[myName])
+            { 
+              for(let x in data[myName])
+              {
+                if(!chats.find((name)=>{ return name==x}))
+                {
+                  chats.push(x);
+                  updatechats();
+                  let fr=[];
+                  fr.push(x);
+                  updatelist(fr);
+                }
+              }
+            }  
+           /* if(!chats.find(people))
              {
               chats.push(people)
               updatechats()
@@ -257,7 +248,7 @@ function card(username,img_src,info){
                       <button id='btn-${username}' class="chat-btns btn btn-outline-dark" onclick="stateChange('${username}')" >
                           <i class='far fa-comment-alt'></i>
                       </button>
-                      <button id='btn-${username}' class="chat-btns btn btn-outline-dark" onclick="stateChange('${username}')" >
+                      <button id='btn--${username}' class="chat-btns btn btn-outline-dark" onclick="stateChange('${username}')" >
                           <i class='fas fa-video'></i>
                       </button>
                   </div>
@@ -273,7 +264,7 @@ function card(username,img_src,info){
                       <button id='btn-${username}' class="chat-btns btn btn-outline-dark" onclick="stateChange('${username}')" >
                           <i class='far fa-comment-alt'></i>
                       </button>
-                      <button id='btn-${username}' class="chat-btns btn btn-outline-dark" onclick="stateChange('${username}')" >
+                      <button id='btn--${username}' class="chat-btns btn btn-outline-dark" onclick="stateChange('${username}')" >
                           <i class='fas fa-video'></i>
                       </button>
                   </div>
@@ -283,7 +274,6 @@ function card(username,img_src,info){
 }
 
 function stateChange(name){
-  console.log(name)
-  globalState=name
-  $("#form")[0].submit()
+  globalState=name;
+  $("#form")[0].submit();
 }
