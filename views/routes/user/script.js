40a3ba4a78737daad738ab9b509d9571e9f1ff69
Paras Nagpal <paras.nag@gmail.com>
2020-03-21 01:53:04 +0530

    (style): UI changes

--- views/routes/user/script.js
+++ views/routes/user/script.js
@@ -2,7 +2,7 @@ let globalState
 
 $(document).ready(()=>{
 
-   $('#form').attr('action','/chats')
+   $("#form").attr("action","/chats")
 
    let socket=io();
    let myName;
@@ -12,18 +12,18 @@ $(document).ready(()=>{
    let userdata={};
    
    //Get Identity from server
-   $.post('identity',{},(data)=>{
+   $.post("identity",{},(data)=>{
       sessionID=data;
-      setCookie('session',data,1);
+      setCookie("session",data,1);
    })
 
-   $.post('myName',
+   $.post("myName",
       {session:sessionID},
       (data)=>{
         myName=data
       })
    .then(()=>{
-    socket.emit('unread');
+    socket.emit("unread");
    })
 
     let newMessageCount={};
@@ -36,31 +36,31 @@ $(document).ready(()=>{
     for(let people of chats)
      {
       newMessageCount[people]=0;
-      $.post('/user',{name:people},(data)=>{
+      $.post("/user",{name:people},(data)=>{
         userdata[people]=data
       })    
      }
-    $('#error').hide();
+    $("#error").hide();
         
     //update List
     updatelist(chats);
      
     //Friend Search
-    $('#friend').keyup((e)=>{
-      $('#error').hide();
+    $("#friend").keyup((e)=>{
+      $("#error").hide();
         if(e.keyCode==13)
           findfriend();
         
     })
-    $('#chatline button').click(()=>{
-      $('#error').hide();      
+    $("#chatline button").click(()=>{
+      $("#error").hide();      
       findfriend();
     })
 
     function findfriend(){
 
-        friend=$('#friend').val()
-        $.post('search',{
+        friend=$("#friend").val()
+        $.post("search",{
           friend
         },(data)=>{
           if(data)
@@ -76,8 +76,8 @@ $(document).ready(()=>{
        
       for(let a of arr) 
       { 
-        let image='https://image.flaticon.com/icons/png/512/37/37943.png';   //hardcoded
-        $('#chats')
+        let image="https://image.flaticon.com/icons/png/512/37/37943.png";   //hardcoded
+        $("#chats")
            .append(card(a,image,userdata[a]));
            
         //check unread
@@ -85,22 +85,22 @@ $(document).ready(()=>{
           if(unreaddata[myName])
             if(unreaddata[myName][a])
             {
-              $('#'+a+" span").html(`<span class="badge badge-pill badge-danger">New Message</span>`);
+              $("#"+a+" span").html(`<span class="badge badge-pill badge-danger">New Message</span>`);
             }
         //Check if photo request has been made earlier
         if(!checkphoto[a])
          {
            //Fetch photo
-            fetch('/photo',{
-                method:'POST',
-                body:JSON.stringify({'name':a}),
+            fetch("/photo",{
+                method:"POST",
+                body:JSON.stringify({"name":a}),
                 headers: {"Content-Type": "application/json"}
               })
             .then(function(response) {
                 if(response.ok) {
                   return response.blob();    //convert response to blob - blob constructor
                 }
-             throw new Error('Network response was not ok.');
+             throw new Error("Network response was not ok.");
              })
              .then(function(myBlob) { 
                     let reader=new FileReader();
@@ -109,24 +109,24 @@ $(document).ready(()=>{
                     checkphoto[a]=reader.result;
               }
              }).catch(function(error) {
-               console.log('There has been a problem with your fetch operation: ', error.message);
+               console.log("There has been a problem with your fetch operation: ", error.message);
             });
          }
          
          if(checkphoto[a])
           {
-            if(checkphoto[a].slice(5,9)!='text')
-              $(`#${a} img`).attr('src',checkphoto[a]);
+            if(checkphoto[a].slice(5,9)!="text")
+              $(`#${a} img`).attr("src",checkphoto[a]);
           }
 
          if(friendsOnline[a])
           {
-            $('#'+a).attr('class','row alert alert-danger mx-3');
-            $(`#btn-`+a).attr('class','chat-btns btn btn-outline-danger');
+            $("#"+a).attr("class","row alert alert-danger");
+            $(`#btn-`+a).attr("class","chat-btns btn btn-outline-danger");
           }
          if(newMessageCount[a])
           {
-            $(`#${a} .friends`).attr('value',a+'    ----'+newMessageCount[a]+'New Messages');
+            $(`#${a} .friends`).attr("value",a+"    ----"+newMessageCount[a]+"New Messages");
           }
       }
     }
@@ -146,22 +146,22 @@ $(document).ready(()=>{
           fr.push(friend);
           updatelist(fr);
       }
-      else $('#error').html('Friend is already in your list!!').show();
+      else $("#error").html("Friend is already in your list!!").show();
 
     }
 
-    $('#form').on('submit',(e)=>{
+    $("#form").on("submit",(e)=>{
        e.preventDefault();
        //set value of hide box
-       $('#send').val(globalState);
-       $('#form')[0].submit();
+       $("#send").val(globalState);
+       $("#form")[0].submit();
     })
     
     //Hide send box
      $("#send").hide();
 
     //Listening to messages
-    socket.on('incoming',(data)=>{
+    socket.on("incoming",(data)=>{
       newMessageCount[data.from]++;
     }) 
 
@@ -169,20 +169,20 @@ $(document).ready(()=>{
      function isOnline()
      {
         for(let people of chats)
-          socket.emit('isOnline',{name:people});
+          socket.emit("isOnline",{name:people});
      }
 
-    socket.on('online',(data)=>{
+    socket.on("online",(data)=>{
        if(data.answer===true)
         friendsOnline[data.name]=true
        else friendsOnline[data.name]=false 
-       $('#chats').html('')
+       $("#chats").html("")
        updatelist(chats)
     })
     
     //Pinging Server that I am online
     setInterval(()=>{
-      socket.emit('isAlive',{
+      socket.emit("isAlive",{
         session:sessionID
       }) 
     },1000)
@@ -193,8 +193,8 @@ $(document).ready(()=>{
 
 
     //get unread data
-    socket.emit('unread') 
-    socket.on('unread',(data)=>{
+    socket.emit("unread") 
+    socket.on("unread",(data)=>{
           unreaddata=data;
           
           if(data)
@@ -239,39 +239,39 @@ function card(username,img_src,info){
   
   if(info)
       return(`
-              <div id='${username}' class='alert alert-light mx-3 row' role='alert' ">
-                  <img src='${img_src}' >
-                  <div class='flex-grow-1 mx-2'>${info.fname+" "+info.lname}</div>
-                  <div class='flex-grow-1 mx-2 '>
-                      <label class='align-self-start'>Username</label>
+              <div id="${username}" class="alert alert-light row" role="alert" ">
+                  <img src="${img_src}" >
+                  <div class="flex-grow-1 mx-2">${info.fname+" "+info.lname}</div>
+                  <div class="flex-grow-1 mx-2 ">
+                      <label class="align-self-start">Username</label>
                       <div style="padding-top:0;">${info.username}</div>
                   </div>
-                  <div class='online' style='display:none;'>
+                  <div class="online" style="display:none;">
                       Online
                   </div>
                   <span ></span>
                   <div >
-                      <button id='btn-${username}' class="chat-btns btn btn-outline-dark" onclick="stateChange('${username}')" >
-                          <i class='far fa-comment-alt'></i>
+                      <button id="btn-${username}" class="chat-btns btn btn-outline-dark" onclick="stateChange("${username}")" >
+                          <i class="far fa-comment-alt"></i>
                       </button>
-                      <button id='btn--${username}' class="chat-btns btn btn-outline-dark" onclick="stateChange('${username}')" >
-                          <i class='fas fa-video'></i>
+                      <button id="btn--${username}" class="chat-btns btn btn-outline-dark" onclick="stateChange("${username}")" >
+                          <i class="fas fa-video"></i>
                       </button>
                   </div>
               </div>
             `)        
   else
       return(`
-              <div id='${username}' class='alert alert-light mx-3 d-flex' role='alert' ">
-                  <img src='${img_src}'>
-                  <div class='flex-grow-1 mx-2'>${username}</div>
+              <div id="${username}" class="alert alert-light d-flex" role="alert" ">
+                  <img src="${img_src}">
+                  <div class="flex-grow-1 mx-2">${username}</div>
                   <span></span>
                   <div>
-                      <button id='btn-${username}' class="chat-btns btn btn-outline-dark" onclick="stateChange('${username}')" >
-                          <i class='far fa-comment-alt'></i>
+                      <button id="btn-${username}" class="chat-btns btn btn-outline-dark" onclick="stateChange("${username}")" >
+                          <i class="far fa-comment-alt"></i>
                       </button>
-                      <button id='btn--${username}' class="chat-btns btn btn-outline-dark" onclick="stateChange('${username}')" >
-                          <i class='fas fa-video'></i>
+                      <button id="btn--${username}" class="chat-btns btn btn-outline-dark" onclick="stateChange("${username}")" >
+                          <i class="fas fa-video"></i>
                       </button>
                   </div>
               </div>
